c80620797fc86865647ffa3699b323e289b755ab
IvoDeK <ivodoetyt@gmail.com>
2023-01-27 16:30:08 +0100

    Fixed possible invalid token error and added new message option

--- commands/prune/prunebans.js
+++ commands/prune/prunebans.js
@@ -5,7 +5,7 @@ export class command {
         bot.cache.set("prunebans", [])
     }
 
-    async run(interaction) {
+    async run(interaction, options) {
         if (bot.cache.get("prunebans").find(guild => guild === interaction.guildId)) return interaction.editReply("Already running...");
 
         let bans = await interaction.guild.bans.fetch();
@@ -18,7 +18,9 @@ export class command {
 
         if (loopAmount > 0) await interaction.editReply(`I'm pruning all bans. This will take ${bans.size === 1000? "more than":"approximately"}: ${toFormatMilliseconds(loopAmount * 3000, true)}`);
 
-        let index = 0
+        let index = 0;
+        let amountOfRefetch = 0;
+        let sendNewMessage = false;
 
         await toLoop();
 
@@ -29,14 +31,19 @@ export class command {
                     interaction.guild.bans.remove(bans.at(index).user.id).catch(err=>err);
                     index++;
                 }
+
                 if (index < bans.size) await toLoop();
                 else {
                     index = 0;
                     bans = await interaction.guild.bans.fetch();
                     if (bans.size === 0) {
-                        await interaction.editReply("All bans have been cleared");
+                        if (options[0]?.value) await interaction.channel.send(`${interaction.user} All bans have been cleared`);
+                        else if (!sendNewMessage) await interaction.editReply("All bans have been cleared");
                         return bot.cache.get("prunebans").splice(bot.cache.get("prunebans").indexOf(interaction.guildId), 1);
                     }
+
+                    amountOfRefetch++;
+                    if (amountOfRefetch === 1) sendNewMessage = bans.size > 400
                     await toLoop();
                 }
             }, loopAmount > 0? 3000:0)
@@ -54,5 +61,11 @@ export const info = {
     ],
     botPermissions: [
         PermissionBits.BAN_MEMBERS
-    ]
+    ],
+    options: [{
+        type: CommandOptionTypes.BOOLEAN,
+        name: "send_end_message",
+        description: "Sends new message in this channel when process is completed. Recommended if you have over 1k bans.",
+        required: false
+    }]
 }
